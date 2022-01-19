4e8496cd78b4578dca356d3bcb69509e4a223d2d
Qphuc <tqp1610@gmail.com>
2022-01-19 15:58:22 +0700

    title website

--- src/router/index.js
+++ src/router/index.js
@@ -4,21 +4,33 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    meta: {
+      title: "Home"
+    },
     component: () => import(/* webpackChunkName: "index" */ "../views/index.vue"),
   },
   {
     path: "/blogs",
     name: "Blogs",
+    meta: {
+      title: "Blogs"
+    },
     component: () => import(/* webpackChunkName: "blogs" */ "../views/blogs.vue"),
   },
   {
     path: "/newpost",
     name: "Newpost",
+    meta: {
+      title: "Newpost"
+    },
     component: () => import(/* webpackChunkName: "newpost" */ "../views/index.vue"),
   },
   {
     path: "/login",
     name: "Login",
+    meta: {
+      title: "Login"
+    },
     component: () => import(/* webpackChunkName: "login" */ "../views/index.vue"),
   },
 ];
@@ -27,5 +39,10 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
+})
 
 export default router;
