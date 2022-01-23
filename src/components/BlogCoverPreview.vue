81954b270138272157a24aec1e7960bc10f7ca6a
Qphuc <tqp1610@gmail.com>
2022-01-23 20:09:44 +0700

    create blogcoer page

---
+++ src/components/BlogCoverPreview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="modal" ref="modal">
+        <div class="modal-content">
+            <i @click="closePreview" class="far fa-times-circle icon"></i>
+            <img :src="this.blogCoverPhoto" alt />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closePreview() {
+            this.$store.commit("openPhotoPreview");
+        }
+    },
+    computed: {
+        blogCoverPhoto() {
+            return this.$store.state.blogPhotoFileURL;
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
