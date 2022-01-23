98dbad6c41f981465e98d44548a89027705ad26d
Qphuc <tqp1610@gmail.com>
2022-01-23 20:14:21 +0700

    css blog coer

--- src/components/BlogCoverPreview.vue
+++ src/components/BlogCoverPreview.vue
@@ -23,4 +23,45 @@ export default {
 </script>
 
 <style scoped>
+.modal {
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 99;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+
+.modal-content {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    border-radius: 12px;
+    background-color: #fff;
+    height: 400px;
+    width: 600px;
+    padding: 50px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+}
+
+.icon {
+    width: 24px;
+    height: auto;
+    cursor: pointer;
+    font-size: 24px;
+    position: absolute;
+    top: 15px;
+    color: #1eb8b8;
+}
+
+.modal-content img {
+    margin-top: 16px;
+    display: block;
+    width: auto;
+    height: 100%;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+}
 </style>
