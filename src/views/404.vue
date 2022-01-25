7b31c545cee08b28b18ea67dc82e102ca2f52c59
Qphuc <tqp1610@gmail.com>
2022-01-25 10:25:16 +0700

    404

---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <p>Error 404: Not Found</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+    width: 1000px;
+    height: 25vh;
+    text-align: center;
+    margin-top: 120px;
+}
+
+p {
+    font-weight: 500;
+    font-size: 24px;
+}
+</style>
